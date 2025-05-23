ab870c8502da54d74691345e49ab407d2910f61a
Gus Masaaki Suga <gus@supercub.one>
2025-05-23 14:04:37 +0900

    install package script

---
+++ 01-install-package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# install yay
+sudo pacman -S git fakeroot binutils make gcc base-devel
+REPO = $HOME/repo
+mkdir $REPO
+cd $REPO
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd $REPO
+
+yay -S --needed - < packages.txt
+
