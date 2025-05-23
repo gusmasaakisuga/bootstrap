c357775935dc4a848f428c44386b2e59d1c87603
Gus Masaaki Suga <gus@supercub.one>
2025-05-23 14:10:37 +0900

    archcraft install script added

--- 01-install-package.sh
+++ 01-install-package.sh
@@ -9,7 +9,66 @@ cd $REPO
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-cd $REPO
 
 yay -S --needed - < packages.txt
+
+
+cd $REPO
+
+cat > install-packages.sh << EOF
+#!/bin/bash
+
+for file in *.zst;
+do
+	echo $file
+	sudo pacman -U --noconfirm $file
+done
+EOF
+
+chmod u+x install-packages.sh
+
+git clone https://github.com/archcraft-os/archcraft-packages.git
+git clone https://github.com/archcraft-os/archcraft-cursors.git
+git clone https://github.com/archcraft-os/archcraft-icons.git
+git clone https://github.com/archcraft-os/archcraft-themes.git
+git clone https://github.com/archcraft-os/archcraft-wallpapers.git
+git clone https://github.com/archcraft-os/archcraft-packages.git
+git clone https://github.com/junegunn/fzf-git.sh.git
+
+cd $REPO/archcraft-cursors
+./build.sh
+cd packages
+cp $REPO/install-packages.sh .
+./install-packages.sh
+cd $REPO
+
+cd archcraft-icons
+./build.sh
+cd packages
+cp $REPO/install-packages.sh .
+./install-packages.sh
+cd $REPO
+
+cd archcraft-themes
+./build.sh
+cd packages
+cp $REPO/install-packages.sh .
+./install-packages.sh
+cd $REPO
+
+cd archcraft-wallpapers
+./build.sh
+cd packages
+cp $REPO/install-packages.sh .
+./install-packages.sh
+cd $REPO
+
+cd archcraft-packages
+rm -rf archcraft-about archcraft-arandr archcraft-help-old archcraft-help-prime archcraft-help-wse archcraft-help archcraft-lxdm archcraft-randr archcraft-welcome
+./build.sh
+cd packages
+cp $REPO/install-packages.sh .
+./install-packages.sh
+
+
 
